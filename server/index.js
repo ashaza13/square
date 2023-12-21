5545e4eeddad685ddffd4a4481421389283e9ef7
ashaza13 <94416376+ashaza13@users.noreply.github.com>
2023-12-21 17:49:14 -0500

    switched crypt

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 require('dotenv/config');
 
